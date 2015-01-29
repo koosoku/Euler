3910119cdb3a5a2aefb4a05981513dc4ebaaab40
Kyle <kylekung@hotmail.com>
2015-01-28 19:48:20 -0500

    fixed triangleSum

--- src/DivisibileTriangleNum.java
+++ src/DivisibileTriangleNum.java
@@ -2,19 +2,44 @@
 public class DivisibileTriangleNum {
 	public static void main(String[] args){
 		long naturalNum=0;
-		int numOfFactors = 0;
-		int len = 16;
+		long copyNaturalNum=0;
+		int numOfFactors = 1;
+		int len = 500; 
 		for(int i =1;numOfFactors<=len;i++){
-			naturalNum+=i;
-			numOfFactors = 0;
+			int[][] recorder = new int[2][10];
+			naturalNum= copyNaturalNum + i;
+			numOfFactors = 1;
+			copyNaturalNum = naturalNum;
 			
-			for(int j =1 ;j<=naturalNum; j++){
+			for(int j =2 ;j<500; j++){
+				if(naturalNum == 1)
+					break;
 				if(naturalNum%j == 0){
-					numOfFactors++;
+					naturalNum = naturalNum/j;
+					for(int k= 0; k<recorder[1].length;k++){
+						if(recorder[0][k] == j){
+							recorder[1][k] ++;
+							j--;
+							break;
+						}
+						if(recorder[0][k] == 0){
+							recorder[0][k] = j;
+							recorder[1][k] ++;
+							j--;
+							break;
+						}
+						
+					}
 				}
 			}
+			
+			int j = 0;
+			while(recorder[0][j]!=0){
+				numOfFactors *= (recorder[1][j]+1);
+				j++;
+			}
 			if(numOfFactors > len){
-				System.out.println(naturalNum);
+				System.out.println(copyNaturalNum);
 				System.out.println(numOfFactors);
 				System.out.print(i);
 				break;
